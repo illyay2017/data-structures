b9707b8cda945fa8998904af2f04c9efc779d1c4
illyay2017 <illya.yanchuk@gmail.com>
2017-11-29 16:09:29 -0800

    solved functional stash

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //initiate count variable at 0;
+  var count = 0;
 
-  // Implement the methods below
   someInstance.push = function(value) {
+    someInstance[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    var poppedValue = someInstance[count - 1];
+    delete someInstance[count - 1];
+    count--;
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    if (count < 0) {
+      return 0;
+    } else {
+      return count;
+    }
   };
-
   return someInstance;
 };
