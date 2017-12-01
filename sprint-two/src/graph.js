05442a9ccfc373a6b3c7d79cc7e084f1f0624c0c
illyay2017 <illya.yanchuk@gmail.com>
2017-12-01 15:36:02 -0800

    solved graph add and contains with new implementation

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,43 +2,50 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  this.nodes = {};
-  this.totalNodes = 0;
-  // this.value = null;
-  // this.edge = null;
+  this.nodes = [];
+  // this.nodes = {};
+  // this.totalNodes = 0;
+  // // this.value = null;
+  // // this.edge = null;
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function(value) {
+  var node = {};
+  node.value = value;
+  node.edges = [];
+  this.nodes.push(node);
   // push the node value and edge into the graph
-  var nodeObj = {};
-  nodeObj.value = node;
-  nodeObj.edges = {};
-  this.nodes[this.totalNodes] = nodeObj;
-  this.totalNodes++;
+  // var nodeObj = {};
+  // nodeObj.value = node;
+  // nodeObj.edges = {};
+  // this.nodes[this.totalNodes] = nodeObj;
+  // this.totalNodes++;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-
-  // Iterate over every node in the storage until the target node was found
-  // if the node is not found return false at the very end
-  for (var key in this.nodes) {
-    return this.nodes[key].value === node;
-  } 
+  for (var i = 0; i < this.nodes.length; i++) {
+    return this.nodes[i].value === node;
+  }
   return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-
+  // for (var key in this.nodes) {
+  //   // if(this.nodes[key].value === node) {
+  //   //   delete this
+  //   // }
+  // }
+  // return false;
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
 };
 
-// Connects two nodes in a graph by adding an edge between them.
+// Connects two nodes in a graph by adding an edge betwesen them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
 };
 
