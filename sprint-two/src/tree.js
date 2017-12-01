cd5c189eef5e15969ea797d0db531b07c5122817
illyay2017 <illya.yanchuk@gmail.com>
2017-11-30 21:49:26 -0800

    solved tree structure!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,33 +1,49 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-  // extend(, newTree);
+  newTree.parent = null;
+  newTree.children = [];
+  extend(newTree, treeMethods);
   return newTree;
 };
 
-// var extend = function(obj, base) {
-//   for (var key in base) {
-//     obj[key] = base[key];
-//   }
-// };
+var extend = function(obj, base) {
+  for (var key in base) {
+    obj[key] = base[key];
+  }
+};
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  childTree.parent = this;
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
+  // var bool = false;
+  // if (this.value === target) {
+  //   bool = true;
+  //   return bool;
+  // } else {
+  //   this.children.forEach(function(childNode) {
+  //     if (childNode.value === target) {
+  //       bool = true;
+  //       return bool;
+  //     } else {
+        
+  //     }
+  //   });
+  // }
+  // return bool;
+  if (this.value === target) {
+    return true;
+  }
+  return _.some(this.children, function(ele) {
+    return ele.contains(target);  
+  });
 };
-
-var TreeNode = function(value) {
-  var node = {};
-  node.value = value;
-  return node;
-};
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
