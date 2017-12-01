329fb79a301f56e518285c03042db1292c942e36
illyay2017 <illya.yanchuk@gmail.com>
2017-12-01 12:27:51 -0800

    solved graph add and contains

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,36 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+  this.totalNodes = 0;
+  // this.value = null;
+  // this.edge = null;
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  // push the node value and edge into the graph
+  var nodeObj = {};
+  nodeObj.value = node;
+  nodeObj.edges = {};
+  this.nodes[this.totalNodes] = nodeObj;
+  this.totalNodes++;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+
+  // Iterate over every node in the storage until the target node was found
+  // if the node is not found return false at the very end
+  for (var key in this.nodes) {
+    return this.nodes[key].value === node;
+  } 
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+this is the new remove
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -36,4 +54,6 @@ Graph.prototype.forEachNode = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+var testGraph = new Graph();
+testGraph.addNode('5');
+console.log(testGraph);
