bd67d9aadbbf602235148bff406de9f66f9e80e9
illyay2017 <illya.yanchuk@gmail.com>
2017-12-01 21:54:42 -0800

    solved Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this.storage[item] === undefined) {
+    this.storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this.storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[item];
 };
 
 /*
